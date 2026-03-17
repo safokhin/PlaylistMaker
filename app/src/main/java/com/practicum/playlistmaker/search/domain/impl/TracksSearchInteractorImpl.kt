9bd0e141fa84ef33348f061092bcafbed2d57d10
artemsafohin <test@test.test>
2026-03-17 16:38:23 +0300

    Спринт 20

--- app/src/main/java/com/practicum/playlistmaker/search/domain/impl/TracksSearchInteractorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/domain/impl/TracksSearchInteractorImpl.kt
@@ -3,12 +3,13 @@ package com.practicum.playlistmaker.search.domain.impl
 import com.practicum.playlistmaker.search.domain.api.TracksSearchInteractor
 import com.practicum.playlistmaker.search.domain.api.TracksSearchRepository
 import com.practicum.playlistmaker.search.domain.models.Track
+import kotlinx.coroutines.flow.Flow
 
 class TracksSearchInteractorImpl(
     private val repository: TracksSearchRepository
 ): TracksSearchInteractor {
 
-    override fun searchTracks(expression: String, callback: (Result<List<Track>>) -> Unit) {
-        repository.searchTracks(expression, callback)
+    override fun searchTracks(expression: String): Flow<Result<List<Track>>> {
+        return repository.searchTracks(expression)
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/search/ui/SearchFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/ui/SearchFragment.kt
@@ -2,8 +2,6 @@ package com.practicum.playlistmaker.search.ui
 
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -16,12 +14,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.FragmentSearchBinding
 import com.practicum.playlistmaker.player.ui.PlayerFragment
 import com.practicum.playlistmaker.search.domain.models.Track
 import com.practicum.playlistmaker.search.domain.models.SearchActivityState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlin.getValue
 
@@ -30,7 +31,6 @@ class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     private val viewModel by viewModel<SearchViewModel>()
     private var isClickAllowed = true
-    private val handler = Handler(Looper.getMainLooper())
 
     private var trackAdapter = TrackAdapter { selectTrackHandler(it) }
     private var trackHistoryAdapter = TrackAdapter { selectTrackHandler(it) }
@@ -56,7 +56,7 @@ class SearchFragment : Fragment() {
 
         val inputMethodManager = requireContext().getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
 
-        viewModel.observeSearchActivity().observe(this) {
+        viewModel.observeSearchActivity().observe(viewLifecycleOwner) {
             renderActivity(it)
         }
 
@@ -189,8 +189,13 @@ class SearchFragment : Fragment() {
 
         if (isClickAllowed) {
             isClickAllowed = false
-            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+
+            lifecycleScope.launch {
+                delay(CLICK_DEBOUNCE_DELAY)
+                isClickAllowed = true
+            }
         }
+
         return current
     }
 
